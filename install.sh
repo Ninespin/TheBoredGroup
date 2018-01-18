4ea93234f8802c9c53f1c09444be255ab8ac4423
KomodoTech <alexandre.leibler@gmail.com>
2018-01-18 10:53:23 -0500

    add support for fedora in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-sudo apt-get install build-essential libsdl1.2debian libsdl1.2-dev libgl1-mesa-dev libglu1-mesa-dev libsdl-image1.2 libsdl-image1.2-dev
-
+sudo apt-get install build-essential libsdl1.2debian libsdl1.2-dev libgl1-mesa-dev libglu1-mesa-dev libsdl-image1.2 libsdl-image1.2-dev || sudo dnf install glew-devel SDL2-devel SDL2_image-devel glm-devel freetype-devel
 
